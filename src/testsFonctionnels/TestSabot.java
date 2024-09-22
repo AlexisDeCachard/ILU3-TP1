a67d33f40d28534dacb8a353c24c7a5dbd090071
AlexisDeCachard <alexisdecachard66740@gmail.com>
2024-09-22 20:25:55 +0200

    TP1 sauf Sabot 2.b et 2.c (question pas comprise)

--- src/testsFonctionnels/TestSabot.java
+++ src/testsFonctionnels/TestSabot.java
@@ -7,7 +7,7 @@ public class TestSabot {
 	
 	public static void main(String[] args) {
 		Cartes carte25=new Borne(25);
-		int nbCartes = 6;
+		int nbCartes = 5;
 		Sabot sabot = new Sabot(nbCartes);
 		for (int i = 0; i<nbCartes;i++) {
 			sabot.ajouterCarte(carte25);
